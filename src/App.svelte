b108ddf5663453707ca407bd09e2bc4ca1bc159e
Verzach3 <gabrielcorreacardenas@gmail.com>
2023-01-13 20:43:37 -0500

    Add: Tailwind, Remove: Boilerplate

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './components/Counter.svelte'
+</script>
+
+<main>
+  <Counter/>
+</main>
+
+<style>
+
+</style>
---
+++ src/components/Counter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let count: number = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button on:click={increment} class="bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-5 rounded-full">
+  count is {count}
+</button>
